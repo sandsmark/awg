d55ff74e26444b5800abb9046fbd4b431126f018
Magnus Fjell <magnuf@users.noreply.github.com>
2008-03-03 23:40:59 +0000

    Created a healer and worker unit

---
+++ src/Healer.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+
+
+public class Healer extends Unit {
+	
+	public int mana;
+
+	public Healer(int faction, int buildingXcoord, int buildingYcoord){
+		xCoord = buildingXcoord +5;
+		yCoord = buildingYcoord +5;
+		mana = 100;
+		maxHealth = 75;
+		CurrentHealth = maxHealth;
+		currentAction = 0;
+		damage = 10;
+		if(faction == 1) setSprite(sprite); //Gløshaugen
+		else(faction ==2)setSprite(sprite); //Dragvoll
+		
+	}
+	
+	
+	public void heal(Unit target){
+		if(mana>10){
+			target.setCurrentHealth(target.getCurrentHealth()+50);
+			this.setMana(getMana()-10);
+		}
+	}
+
+
+	public int getMana() {
+		return mana;
+	}
+
+
+	public void setMana(int mana) {
+		this.mana = mana;
+	}
+}
--- src/Worker.java
+++ src/Worker.java
@@ -1,8 +1,38 @@
+import java.io.IOException;
+
 
 public class Worker extends Unit {
 //	Bevegelses- og idrettsvitenskap for Dragvoll
 //	Bygg og miljøteknikk for Dragvoll
 	
-	public int carryingCapacity; //How much resources the worker can carry
+	public int carrying; //How much resources the worker is carrying, when == 10, go home or something
+
+	public Worker(int faction, int mainHouseXcoord, int mainHouseYcoord){
+		maxHealth = 50;
+		CurrentHealth = maxHealth;
+		damage = 5;
+		xCoord = mainhouse coord + 0;
+		yCoord = mainHouse coord + 5;
+		range = 25;
+		currentAction = 0;
+		if(faction == 1) setSprite(sprite); //Gløshaugen
+		else if(faction ==2)setSprite(sprite); //Dragvoll
+	}
 	
+	public void deliverResource(){
+		//GOTO mainhouse
+		if(xCoord == mainhouse coord +- 25 || yCoord == mainhousecoord +-25){
+			int delivered = getCarrying();
+			//increase resource counter with delivered
+			setCarrying(0);
+		}
+	}
+
+	public int getCarrying() {
+		return carrying;
+	}
+
+	public void setCarrying(int carrying) {
+		this.carrying = carrying;
+	}
 }
