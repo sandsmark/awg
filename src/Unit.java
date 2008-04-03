675798a9a1ae668ec56dad39491247b1531021d3
martin <martin@sandsmark.ninja>
2008-04-03 15:39:23 +0000

    fixed up a bit

--- src/Unit.java
+++ src/Unit.java
@@ -95,13 +95,14 @@ public class Unit {
 		float distY = target.y - position.y;
 		if (distX == 0) distX = 1;
 		orientation = Math.atan2(distY, distX);
-		System.out.println(orientation);
-		orientation += Math.PI;
+		
+		orientation += Math.PI; // TODO: Ugly hack
 		if (orientation < Math.PI/4 || orientation > 7 * Math.PI / 4) sprite.setDirection(Sprite.Direction.LEFT);
 		else if (orientation > Math.PI/4 && orientation < 3 * Math.PI / 4) sprite.setDirection(Sprite.Direction.BACK);
 		else if (orientation >  3 * Math.PI/4 && orientation < 5 * Math.PI / 4) sprite.setDirection(Sprite.Direction.RIGHT);
 		else if (orientation >  5 * Math.PI/4 && orientation < 7 * Math.PI / 4) sprite.setDirection(Sprite.Direction.FORWARD);
 		orientation -= Math.PI;
+		
 		speed = 0;
 	}
 	
