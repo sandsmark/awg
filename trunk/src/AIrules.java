ff92e9e5a338d030bc5c1eb3476c0983737b24b6
themorn <themorn@122933f3-7447-0410-9fd8-1f29c74ce1d2>
2008-04-04 11:01:48 +0000

    AIregler, ikke implementert

--- trunk/src/AIrules.java
+++ trunk/src/AIrules.java
@@ -1,64 +1,80 @@
 
 public class AIrules {
 
-	double UnitSenseRange; //radius for sansevidde
-	int AttackForce; //hvor mange units før attack
-	int Workers; //hvor mange workers han opererer med
-	double BaseSenseRange; //hvor nære basen før han forsvarer
-	int Aggro; //hvor ofte han sender wave i sekunder
-	int Defence; //hvor mange units i forsvar
-	int FighterPerHealer;
+	double unitSenseRange; //radius for sansevidde
+	int attackForce; //hvor mange units før attack
+	int workers; //hvor mange workers han opererer med
+	double baseSenseRange; //hvor nære basen før han forsvarer
+	int aggro; //hvor ofte han sender wave i sekunder
+	int defence; //hvor mange units i forsvar
+	int fighterPerHealer; //healer/fighter ratio
+	int startGold; //start gull
+	int upgrade; // når han skal oppgradere hovedbygg i sek
 	
 	public AIrules() {
-		this.UnitSenseRange=15;
-		this.AttackForce = 6;
-		this.Workers = 5;
-		this.BaseSenseRange = 30;
-		this.Aggro = 36000; //i sek
-		this.Defence = 5;
-		this.FighterPerHealer = 5;
+		this.unitSenseRange=15;
+		this.attackForce = 6;
+		this.workers = 5;
+		this.baseSenseRange = 30;
+		this.aggro = 36000; //i sek
+		this.defence = 5;
+		this.fighterPerHealer = 5;
+		this.startGold = 50000;
+		this.upgrade = 108000; //i sek
 	}
-	public void setUnitSenseRange(double d) {
-		this.UnitSenseRange = d;
+	public void setunitSenseRange(double d) {
+		this.unitSenseRange = d;
 	}
 	public void setAttackForce(int af) {
-		this.AttackForce = af;
+		this.attackForce = af;
 	}
 	public void setWorkers(int w) {
-		this.Workers = w;
+		this.workers = w;
 	}
 	public void setBaseSenseRange(double b) {
-		this.BaseSenseRange = b;
+		this.baseSenseRange = b;
 	}
 	public void setAggro(int a) {
-		this.UnitSenseRange = a;
+		this.aggro = a;
 	}
 	public void setDefence(int d) {
-		this.UnitSenseRange = d;
+		this.defence = d;
 	}
 	public double getUnitSenseRange() {
-		return this.UnitSenseRange;
+		return this.unitSenseRange;
 	}
 	public int getAttackForce() {
-		return this.AttackForce;
+		return this.attackForce;
 	}
 	public int getWorkers() {
-		return this.Workers;
+		return this.workers;
 	}
 	public double getBaseSenseRange() {
-		return this.BaseSenseRange;
+		return this.baseSenseRange;
 	}
 	public int getAggro() {
-		return this.Aggro;
+		return this.aggro;
 	}
 	public int getDefence() {
-		return this.Defence;
+		return this.defence;
 	}
-	public int getFighterPerHealer() {
-		return this.FighterPerHealer;
+	public int getfighterPerHealer() {
+		return this.fighterPerHealer;
 	}
-	public void setFighterPerHealer(int fighterPerHealer) {
-		this.FighterPerHealer = fighterPerHealer;
+	public void setfighterPerHealer(int fighterPerHealer) {
+		this.fighterPerHealer = fighterPerHealer;
+	}
+	public int getStartGold() {
+		return this.startGold;
+	}
+	public void setStartGold(int startGold) {
+		this.startGold = startGold;
+	}
+	public int getUpgrade() {
+		return this.upgrade;
+	}
+	public void setUpgrade(int upgrade) {
+		this.upgrade = upgrade;
 	}
 	
 	
