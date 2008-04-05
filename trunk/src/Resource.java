3c2852e1c49bbca9339750f6ffff16d430d0f818
martin.sandsmark <martin.sandsmark@122933f3-7447-0410-9fd8-1f29c74ce1d2>
2008-04-05 14:40:38 +0000

    highlight resource when selected.

--- trunk/src/Resource.java
+++ trunk/src/Resource.java
@@ -9,13 +9,16 @@ public class Resource {
 	protected int resourcesLeft;
 	protected BufferedImage sprite;
 	protected Point position;
-	protected long blinking;
+	protected BufferedImage selectedSprite;
+	protected long highlightTime;
 	
 	public Resource(int initialVal, int x, int y) {
 		resourcesLeft = initialVal;
 		position = new Point(x,y);
 		try {
-			sprite = ImageIO.read(new File("resources/rocks/" + (int)(Math.random()*2) + ".png"));
+			int num = (int)(Math.random()*2);
+			sprite = ImageIO.read(new File("resources/rocks/" + num + ".png"));
+			selectedSprite = ImageIO.read(new File("resources/rocks/" + num + "s.png")); 
 		} catch (IOException e) {
 			System.err.println("Could not load resource sprite!");
 			System.exit(1);
@@ -36,7 +39,9 @@ public class Resource {
 	}
 
 	public BufferedImage getSprite() {
-		return sprite;
+		if (System.currentTimeMillis() - highlightTime > 1000) highlightTime = 0;
+		if (highlightTime != 0) return selectedSprite;
+		else return sprite;
 	}
 
 	public Point getPosition() {
@@ -47,8 +52,8 @@ public class Resource {
 		return resourcesLeft;
 	}
 	
-	public void startBlink() {
-		blinking = System.currentTimeMillis();
+	public void startHighlight() {
+		highlightTime = System.currentTimeMillis();
 	}
 	
 }
