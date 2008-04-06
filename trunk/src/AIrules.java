15bd220e216e910d6ecfb3ca72702217934fbd8e
themorn <themorn@122933f3-7447-0410-9fd8-1f29c74ce1d2>
2008-04-06 13:23:52 +0000

    AIregler, ikke implementert

--- trunk/src/AIrules.java
+++ trunk/src/AIrules.java
@@ -5,22 +5,22 @@ public class AIrules {
 	int attackForce; //hvor mange units før attack
 	int workers; //hvor mange workers han opererer med
 	double baseSenseRange; //hvor nære basen før han forsvarer
-	int aggro; //hvor ofte han sender wave i sekunder
+	long aggro; //hvor ofte han sender wave i ms
 	int defence; //hvor mange units i forsvar
 	int fighterPerHealer; //healer/fighter ratio
 	int startGold; //start gull
-	int upgrade; // når han skal oppgradere hovedbygg i sek
+	long upgrade; // når han skal oppgradere hovedbygg i ms
 	
 	public AIrules() {
 		this.unitSenseRange=15;
 		this.attackForce = 6;
 		this.workers = 5;
 		this.baseSenseRange = 30;
-		this.aggro = 36000; //i sek
+		this.aggro = 36000000; //i ms
 		this.defence = 5;
 		this.fighterPerHealer = 5;
 		this.startGold = 50000;
-		this.upgrade = 108000; //i sek
+		this.upgrade = 108000000; //i ms
 	}
 	public void setunitSenseRange(double d) {
 		this.unitSenseRange = d;
@@ -34,7 +34,7 @@ public class AIrules {
 	public void setBaseSenseRange(double b) {
 		this.baseSenseRange = b;
 	}
-	public void setAggro(int a) {
+	public void setAggro(long a) {
 		this.aggro = a;
 	}
 	public void setDefence(int d) {
@@ -52,7 +52,7 @@ public class AIrules {
 	public double getBaseSenseRange() {
 		return this.baseSenseRange;
 	}
-	public int getAggro() {
+	public long getAggro() {
 		return this.aggro;
 	}
 	public int getDefence() {
@@ -70,10 +70,10 @@ public class AIrules {
 	public void setStartGold(int startGold) {
 		this.startGold = startGold;
 	}
-	public int getUpgrade() {
+	public long getUpgrade() {
 		return this.upgrade;
 	}
-	public void setUpgrade(int upgrade) {
+	public void setUpgrade(long upgrade) {
 		this.upgrade = upgrade;
 	}
 	
