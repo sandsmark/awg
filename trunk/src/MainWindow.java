1da70e5f79b07e5fe391222dd0c92486ec60e763
martin.sandsmark <martin.sandsmark@122933f3-7447-0410-9fd8-1f29c74ce1d2>
2008-04-11 23:58:55 +0000

    get rid of annoying flicker, and add some more sprites.

--- trunk/src/MainWindow.java
+++ trunk/src/MainWindow.java
@@ -127,11 +127,8 @@ public class MainWindow implements ActionListener, MouseMotionListener,
 		
 
 		// Add testing unit
-//		GameState.getUnits().addUnit(new Worker(GameState.getComputer())); AI gj�r dette selv
 		GameState.getUnits().addUnit(new Worker(GameState.getHuman()));
-//		GameState.getUnits().addUnit(new Fighter(GameState.getComputer())); AI gj�r dette selv
-		
-		canvas.updateInternal(); // Should be called whenever the map updates
+	
 		splash.destroy();
 		frame.setVisible(true);
 	}
