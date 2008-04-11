d8664df6fae9fec1b671ed67f2afe7a987d58ba1
martin.sandsmark <martin.sandsmark@122933f3-7447-0410-9fd8-1f29c74ce1d2>
2008-04-11 11:58:34 +0000

    we has teh singleton config.

--- trunk/src/Config.java
+++ trunk/src/Config.java
@@ -3,70 +3,45 @@ public class Config {
 	/*
 	 * 2000x2000 breaks the ai, for unknown reasons
 	 */
-	private static int worldWidth = 3000; // x pixels wide
-	private static int worldHeight = 3000; // y pixels high
-	private static int unitWidth = 25; //Ditto
-	private static int unitHeight = 25;
-	private static int sleeptime = 50; // in ms.
+	private int worldWidth = 1500; // x pixels wide
+	private int worldHeight = 1500; // y pixels high
+	private int unitWidth = 25; //Ditto
+	private int unitHeight = 25;
+	private int sleeptime = 50; // in ms.
 
+	
+	private static class ConfigHolder {
+		private final static Config config = new Config();
+	}
+	
 	/**
 	 * @return the worldWidth
 	 */
 	public static int getWorldWidth() {
-		return worldWidth;
+		return ConfigHolder.config.worldWidth;
 	}
 	/**
 	 * @return the worldHeight
 	 */
 	public static int getWorldHeight() {
-		return worldHeight;
+		return ConfigHolder.config.worldHeight;
 	}
 	/**
 	 * @return the unitWidth
 	 */
 	public static int getUnitWidth() {
-		return unitWidth;
+		return ConfigHolder.config.unitWidth;
 	}
 	/**
 	 * @return the unitHeight
 	 */
 	public static int getUnitHeight() {
-		return unitHeight;
+		return ConfigHolder.config.unitHeight;
 	}
 	/**
 	 * @return the sleeptime
 	 */
 	public static int getSleeptime() {
-		return sleeptime;
-	}
-	/**
-	 * @param worldWidth the worldWidth to set
-	 */
-	public static void setWorldWidth(int worldWidth) {
-		Config.worldWidth = worldWidth;
-	}
-	/**
-	 * @param worldHeight the worldHeight to set
-	 */
-	public static void setWorldHeight(int worldHeight) {
-		Config.worldHeight = worldHeight;
-	}
-	/**
-	 * @param unitWidth the unitWidth to set
-	 */
-	public static void setUnitWidth(int unitWidth) {
-		Config.unitWidth = unitWidth;
-	}
-	/**
-	 * @param unitHeight the unitHeight to set
-	 */
-	public static void setUnitHeight(int unitHeight) {
-		Config.unitHeight = unitHeight;
-	}
-	/**
-	 * @param sleeptime the sleeptime to set
-	 */
-	public static void setSleeptime(int sleeptime) {
-		Config.sleeptime = sleeptime;
+		return ConfigHolder.config.sleeptime;
 	}
 }
