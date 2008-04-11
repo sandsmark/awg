d68fc6cf9fb79a183ebf565b241cef50dc10d3f5
martin <martin@sandsmark.ninja>
2008-04-11 16:34:51 +0000

    try fullscreen.

--- src/Intro.java
+++ src/Intro.java
@@ -56,34 +56,13 @@ public class Intro extends JWindow implements MouseListener {
 		} catch (Exception e) { e.printStackTrace(); }
 	}
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		lock.lock();
-		clicked.signal();
-		lock.unlock();
-	}
+	public void mouseClicked(MouseEvent e) {	}
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void mouseEntered(MouseEvent e) { }
 
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void mouseExited(MouseEvent e) { 	}
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void mousePressed(MouseEvent e) {	}
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void mouseReleased(MouseEvent e) {	}
 }
