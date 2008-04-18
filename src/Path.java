5c44258a7dc26071d96aabd9b6f83311941c392e
martin <martin@sandsmark.ninja>
2008-04-18 10:23:32 +0000

    fiex

--- src/Path.java
+++ src/Path.java
@@ -126,7 +126,7 @@ public class Path implements Comparable<Path> {
 			for (int dx = -1; dx <= 1; dx++)
 				for (int dy = -1; dy <= 1; dy ++)
 					if (!(dx == 0 && dy == 0)) 
-						if (m.canMove((point.x + dx) * factor, (point.y + dy) * factor))
+						if (m.canMove(((point.x + dx) * factor) + factor, ((point.y + dy) * factor) + factor))
 							if (path.getLength() < Config.getWorldHeight() + Config.getWorldWidth())
 								queue.add(new Path(path, map[point.x + dx][point.y + dy], target));	
 		}
