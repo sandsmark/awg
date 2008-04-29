ae30cad5bb2a3e177afe125a5251652a757c1e45
martin.sandsmark <martin.sandsmark@122933f3-7447-0410-9fd8-1f29c74ce1d2>
2008-04-29 21:52:33 +0000

    Commenting and sane naming.

--- trunk/src/Sprite.java
+++ trunk/src/Sprite.java
@@ -1,16 +1,37 @@
+/*
+Copyright (C) 2008 Martin T. Sandsmark
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-
+/**
+ * This holds the sprites of units.
+ * @author sandsmark
+ */
 public class Sprite {
 	public enum Direction {
-		BACK, FORWARD, RIGHT, LEFT
+		UP, DOWN, RIGHT, LEFT
 	}
 	int fps = 10;
 	
-	private Direction direction = Direction.FORWARD;
+	private Direction direction = Direction.DOWN;
 	private int cycle = 0;
 	private boolean isMoving = false;
 	private long isHit = -1;
@@ -23,7 +44,7 @@ public class Sprite {
 	private int faction;
 	private String[] directionNames = {"back", "forward", "right", "left" };
 	
-	/**
+	/*
 	 *  sprite[0-1] = faction 0, 1
 	 *  sprite[][0-3], direction forward,backward,right,left
 	 *  sprite[][][0-1], part of walk cycle
@@ -31,8 +52,13 @@ public class Sprite {
 	 *   \0 = normal, 1 = being attacked, 2 = doing stuff
 	 */
 	private BufferedImage[][][][] sprite = new BufferedImage[2][4][2][3];
-	
-	public Sprite (String basename, int faction) { // Basename contains either "fighter", "healer" or "worker"
+	/**
+	 * This takes in the name of the unit type, and the faction the unit belongs to.
+	 * Then it loads in the appropriate sprites from image files.
+	 * @param basename contains either "fighter", "healer" or "worker".
+	 * @param faction defines which faction (team/color) the unit is.
+	 */
+	public Sprite (String basename, int faction) {
 		try {			
 			for (int f=0; f<2; f++){
 				for (int dir=0; dir<4; dir++) {
@@ -60,6 +86,10 @@ public class Sprite {
 		}
 	}
 	
+	/**
+	 * This returns and removes the next sprite in the walk cycle.
+	 * @return
+	 */
 	public BufferedImage pop() {
 		if (System.currentTimeMillis() - last > 1000/fps) {
 			if (!isMoving && !isDoing) cycle = 1;
@@ -72,30 +102,55 @@ public class Sprite {
 		else return sprite[faction][direction.ordinal()][cycle][1];
 	}
 
+	/**
+	 * This sets which direction the unit is pointing.
+	 * @param direction
+	 */
 	public void setDirection(Direction direction) {
 		this.direction = direction;
 	}
 
+	/**
+	 * This just returns the current sprite. 
+	 */
 	public BufferedImage get() {
 		return sprite[faction][direction.ordinal()][cycle][0];
 	}
 	
+	/**
+	 * This sets if the sprite should appear to be walking or not.
+	 * @param moving
+	 */
 	public void setMoving(boolean moving) {
 		this.isMoving = moving;
 	}
 	
+	/**
+	 * This gets called when the unit gets damaged.
+	 */
 	public void hit() {
 		isHit = System.currentTimeMillis();
 	}
 	
+	/**
+	 * This sets if the unit is doing anything (killing, mining, healing, et c.)
+	 * @param isDoing
+	 */
 	public void setDoing(boolean isDoing) {
 		this.isDoing = isDoing;
 	}
 	
+	/**
+	 * This returns the width of the sprite.
+	 */
 	public int getWidth() {
 		return sprite[0][0][0][0].getWidth(); 
 	}
 	
+	/**
+	 * This returns the height of the sprite.
+	 * @return
+	 */
 	public int getHeight() {
 		return sprite[0][0][0][0].getHeight();
 	}
